8322aae2f1953b9b9a098b6ae22a5d6bf6c639c0
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-11 14:31:46 -0400

    adds ability to check version and bumps version

--- lib/git-sleep.rb
+++ lib/git-sleep.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'httparty'
 
 module GitSleep
-  VERSION      = "0.0.6"
+  VERSION      = "0.0.7"
   LAST_UPDATED = "2013-08-11"
   NETRC_PATH   = File.expand_path('~/.netrc')
   OUR_SITE     = "http://sleepytime.com"
