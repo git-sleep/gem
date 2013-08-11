64ea0a3bcf8948696dbf717af2ff9501a9496dcf
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-11 15:12:58 -0400

    dumfix

--- lib/git-sleep.rb
+++ lib/git-sleep.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'httparty'
 
 module GitSleep
-  VERSION      = "0.0.11"
+  VERSION      = "0.0.12"
   LAST_UPDATED = "2013-08-11"
   NETRC_PATH   = File.expand_path('~/.netrc')
   OUR_SITE     = "http://sleepytime.com"
