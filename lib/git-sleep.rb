aacb463f463b180cedd9eaf10a927a267009fbaf
Max Jacobson <max@hardscrabble.net>
2014-11-15 19:46:51 -0500

    bump version to v0.2.0

--- lib/git-sleep.rb
+++ lib/git-sleep.rb
@@ -2,7 +2,7 @@ require_relative './git-sleep/exceptions'
 require_relative './git-sleep/authorizer'
 
 module GitSleep
-  VERSION      = '0.1.1'
+  VERSION      = '0.2.0'
   OUR_SITE     = 'http://www.gitsleep.com'
 end
 
